6098ee25a3950da68f0ce00753fb1b9a9de0fb91
michaelDonchenko <michael1994d@gmail.com>
2020-10-18 11:32:18 +0300

    make it better

--- server.js
+++ server.js
@@ -19,9 +19,18 @@ app.use('/auth', authRoutes)
 //connect DB
 connectDB()
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 const PORT = process.env.PORT || 5000
 
 //port listener
 app.listen(PORT, () => {
   console.log(`The app listening at http://localhost:${PORT}`)
 })
+
